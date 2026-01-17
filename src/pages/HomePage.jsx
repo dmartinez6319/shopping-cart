dddccc145c3dde0853d3da23868fa5601e3e9da7
daniel <dmartinez6319@gmail.com>
2026-01-17 03:06:55 -0500

    homepage styles and functionality

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,22 @@
+import styles from "../styles/Homepage.module.css"
+import { Link } from "react-router";
+import shopperImg from "../assets/shopper.png"
+
 const Homepage = () => {
     return (
         <>
-        home
+        
+            <main className={styles.main}>
+                <div className={styles.displayText}>
+                    <h1>Your all-in-one shopping destination!</h1>
+                    <Link to="store" className={styles.shopButton}>Shop Now</Link>
+                </div>
+                <div className={styles.displayImage}>
+                    
+                    <img src={shopperImg} alt="shopper icon" />
+                </div>
+            </main>
+
         </>
     )
 }
